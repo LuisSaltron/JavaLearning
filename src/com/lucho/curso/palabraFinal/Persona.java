f68d5ffa50f7202503f9937f920ed2e77724e597
Luis Saltrón <luis.saltron2@gmail.com>
2021-08-05 21:32:49 -0500

    * Added exercises using reserverd final to variables, methods & clases

---
+++ src/com/lucho/curso/palabraFinal/Persona.java
@@ -0,0 +1,22 @@
+package com.lucho.curso.palabraFinal;
+
+public class Persona {
+	private String nombre;
+	public final static int NUM=12;
+	public final void imprimir() {
+		System.out.println("Metodo final imprimir");
+	}
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+}
