49d4700542a7fd50fa784d430eb1c93fff850458
Luis Saltrón <luis.saltron2@gmail.com>
2021-08-05 21:08:46 -0500

    * Added exercises for methods & constructors overload * Added exercises for package use & imports

---
+++ src/com/lucho/curso/herencia/Cliente.java
@@ -0,0 +1,39 @@
+package com.lucho.curso.herencia;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Cliente extends Persona {
+	private int idCliente;
+	private Date fechaRegistro;
+	private boolean vip;
+	private static int contadorCliente;
+
+	public Cliente(String nombre, char genero, int edad, String direccion, String fecha, boolean vip) {
+		super(nombre,genero,edad,direccion);
+		this.idCliente = ++Cliente.contadorCliente;
+		try {
+			this.fechaRegistro = new SimpleDateFormat("dd/MM/yyyy").parse(fecha);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		this.vip = vip;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Cliente [idCliente=");
+		builder.append(idCliente);
+		builder.append(", fechaRegistro=");
+		builder.append(fechaRegistro);
+		builder.append(", vip=");
+		builder.append(vip);
+		builder.append(", toString()=");
+		builder.append(super.toString());
+		builder.append("]");
+		return builder.toString();
+	}
+}
---
+++ src/com/lucho/curso/herencia/Empleado.java
@@ -0,0 +1,60 @@
+package com.lucho.curso.herencia;
+
+public class Empleado extends Persona {
+	private int idEmpleado;
+	private double sueldo;
+	private static int contadorEmpleado;
+	
+	public Empleado(){
+		this.idEmpleado=++Empleado.contadorEmpleado;
+	}
+	public Empleado(String nombre, double sueldo) {
+		this();
+		this.nombre=nombre; 
+		this.sueldo = sueldo;
+	}
+
+	public Empleado(String nombre) {
+		super(nombre);
+		this.idEmpleado=++Empleado.contadorEmpleado;
+	}
+
+	public Empleado(String nombre, char genero, int edad, String direccion) {
+		super(nombre, genero, edad, direccion);
+		Empleado.contadorEmpleado++;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Empleado [idEmpleado=");
+		builder.append(idEmpleado);
+		builder.append(", sueldo=");
+		builder.append(sueldo);
+		builder.append(super.toString());
+		builder.append("]");
+		return builder.toString();
+	}
+
+	/**
+	 * @return the idEmpleado
+	 */
+	public int getIdEmpleado() {
+		return idEmpleado;
+	}
+
+	/**
+	 * @return the sueldo
+	 */
+	public double getSueldo() {
+		return sueldo;
+	}
+
+	/**
+	 * @param sueldo the sueldo to set
+	 */
+	public void setSueldo(double sueldo) {
+		this.sueldo = sueldo;
+	}
+	
+}
