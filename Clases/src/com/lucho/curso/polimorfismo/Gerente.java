21a70ed9ed6a9e96d9775409dceee3b411ff536a
Luis Saltron <luis.saltron@skaphe.com>
2021-08-16 18:55:50 -0500

    * polymorphism exercises added

---
+++ Clases/src/com/lucho/curso/polimorfismo/Gerente.java
@@ -0,0 +1,30 @@
+package com.lucho.curso.polimorfismo;
+
+public class Gerente extends Empleado {
+	private String departamento;
+	
+	public Gerente (String nombre, double sueldo, String departamento) {
+		super(nombre,sueldo);
+		this.departamento=departamento;
+	}
+	@Override
+	public String obtenerDetalles() {
+		// TODO Auto-generated method stub
+		return super.obtenerDetalles()+", Departamento "+this.departamento;
+	}
+	
+	/**
+	 * @return the departamento
+	 */
+	public String getDepartamento() {
+		return departamento;
+	}
+
+	/**
+	 * @param departamento the departamento to set
+	 */
+	public void setDepartamento(String departamento) {
+		this.departamento = departamento;
+	}
+	
+}
