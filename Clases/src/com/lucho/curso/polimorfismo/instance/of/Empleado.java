b7fe0a61cdc9a7ea314750ce07ee5b3df6bfe5c4
Luis Saltrón <luis.saltron2@gmail.com>
2021-08-31 16:45:22 -0500

    * Instance of exercises added

---
+++ Clases/src/com/lucho/curso/polimorfismo/instance/of/Empleado.java
@@ -0,0 +1,44 @@
+package com.lucho.curso.polimorfismo.instance.of;
+
+public class Empleado {
+	protected String nombre;
+	protected double sueldo;
+
+	public Empleado(String nombre, double sueldo) {
+		this.nombre = nombre;
+		this.sueldo = sueldo;
+	}
+
+	protected String obtenerDetalles() {
+		return "Nombre " + this.nombre + ", Sueldo " + this.sueldo;
+	}
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	/**
+	 * @return the sueldo
+	 */
+	public double getSueldo() {
+		return sueldo;
+	}
+
+	/**
+	 * @param sueldo the sueldo to set
+	 */
+	public void setSueldo(double sueldo) {
+		this.sueldo = sueldo;
+	}
+
+}
