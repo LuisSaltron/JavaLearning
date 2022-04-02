f8aa0f8503d6401c73ce0b1396da4b6664b00780
luis.saltron <luis.saltron2@gmail.com>
2022-04-01 21:48:12 -0500

    * First hello world exercise regarding Java Servlets

---
+++ Servlets/HolaMundoJavaWeb/src/main/java/web/HolaMundo.java
@@ -0,0 +1,21 @@
+package web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+@WebServlet("/HolaMundo")
+public class HolaMundo extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = resp.getWriter();
+		out.println("Hola mundo desde servlets");
+	}
+}
